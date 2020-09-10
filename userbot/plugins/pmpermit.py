61beec99653db9b1efcb60cee27722bb46b29c66
tow3rr <69804065+tow3rr@users.noreply.github.com>
2020-09-10 15:46:19 +0200

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -10,7 +10,7 @@ from userbot.utils import admin_cmd
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
-  WARN_PIC = "https://telegra.ph/file/82c595986872349e5ba1a.jpg"
+  WARN_PIC = "http://prnt.sc/ueyboz"
 else:
   WARN_PIC = PMPERMIT_PIC
 
@@ -19,14 +19,13 @@ PREV_REPLY_MESSAGE = {}
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-CUSTOM_MIDDLE_PMP = str(CUSTOM_PMPERMIT) if CUSTOM_PMPERMIT else "**YOU HAVE TRESPASSED TO MY MASTERS INBOX** \n`THIS IS ILLEGAL AND REGARDED AS A CRIME`"
-USER_BOT_WARN_ZERO = "`You were spamming my Boss's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.` "
-USER_BOT_NO_WARN = ("`Hello ! This is` **F.R.I.D.A.Y**\n"
-                    "`Private Messaging Security Protocol ⚠️`\n\n"
-                    "**Currently My Boss**\n"
-                    f"{DEFAULTUSER} is Busy ! So Better Don't Spam His Inbox !\n\n"
+CUSTOM_MIDDLE_PMP = str(CUSTOM_PMPERMIT) if CUSTOM_PMPERMIT else "**Ciao`"
+USER_BOT_WARN_ZERO = "`Stai spammando, sei stato bloccato.` "
+USER_BOT_NO_WARN = ("`Ciao, sei nella mia gui**\n"  
+                    "**Dirò al mio capo**\n"
+                    f"{DEFAULTUSER} di risponderti appena può ! Ora per aiutarlo !\n\n"
                     f"{CUSTOM_MIDDLE_PMP} \n\n"
-                    "**Now You Are In Trouble So Send** 🍁 `/start` 🍁  **To Start A Valid Conversation!!**")
+                    "**Scrivi** 🍁 `/start` 🍁  **Per aiutare il mio Capo!!**")
 
 
 if Var.PRIVATE_GROUP_ID is not None:
