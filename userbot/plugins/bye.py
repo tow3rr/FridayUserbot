e0e479ccc626bce96c0fb1671d448a27af63ee71
tow3rr <69804065+tow3rr@users.noreply.github.com>
2020-09-10 15:31:44 +0200

    Update bye.py

--- userbot/plugins/bye.py
+++ userbot/plugins/bye.py
@@ -11,11 +11,11 @@ import time
 @borg.on(admin_cmd("bye", outgoing=True))
 @borg.on(sudo_cmd("bye", allow_sudo=True))
 async def leave(e):
-    starkgang = await edit_or_reply(e, "Bye Kek")
+    starkgang = await edit_or_reply(e, "Addio coglioni")
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await starkgang.edit("`I am leaving this chat.....!`")
+        await starkgang.edit("`Sto uscendo dalla chat, fottetevi!`")
         time.sleep(3)
         if '-' in str(e.chat_id):
             await borg(LeaveChannelRequest(e.chat_id))
         else:
-            await starkgang.edit('`Sir This is Not A Chat`')
+            await starkgang.edit('`Smettila di trollare`')
